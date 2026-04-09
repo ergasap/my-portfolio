f38f05357700d0ef62d191eecbe92c99957e219a
Ernesto Gaspar <ergasap@MacBook-Air-de-Ernesto.local>
2026-04-09 10:21:03 +0200

    initial commit - clean and minimalist portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NAME: "My Portfolio",
+  EMAIL: "ernestogaspar2000@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 1,
+  NUM_WORKS_ON_HOMEPAGE: 1,
+  NUM_PROJECTS_ON_HOMEPAGE: 1,
 };
 
 export const HOME: Metadata = {
@@ -30,15 +30,7 @@ export const PROJECTS: Metadata = {
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
-  { 
-    NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
-  },
-  { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/ernesto-gaspar-aparicio-a56404214/",
   }
 ];
---
+++ src/content/blog/01-devops-project/index.md
@@ -0,0 +1,10 @@
+---
+title: "Architecting a Production-Ready Infrastructure"
+description: "Integrating AWS, Terraform, and Kubernetes"
+date: "2026-04-08"
+draft: false
+---
+
+Aquí empieza tu contenido técnico...
+
+Hola 
